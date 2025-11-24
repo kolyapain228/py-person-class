ca451d495f701b408bc6f4820eab3f2bdcc948ce
Mykola Mosiienko <koljamos@gmail.com>
2025-11-24 16:17:00 +0200

    Created calss Person and create_person_list func

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for person in people:
+        result.append(Person(person['name'], person['age']))
+    for p in people:
+        obj = Person.people[p['name']]
+        if p["wife"] in Person.people:
+            obj.wife = Person.people[p["wife"]]
+        if p["husband"] in Person.people:
+            obj.husband = Person.people[p["husband"]]
+    return result
